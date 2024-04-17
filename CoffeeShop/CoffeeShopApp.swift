55e43d529b0cda855341fb5dda7cd6d1a0e47433
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-04-17 13:13:28 +0700

    Initial Commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by MACBOOK PRO on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
